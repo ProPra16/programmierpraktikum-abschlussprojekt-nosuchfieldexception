b8a143975794bc9779f68c226bb8c2923a1b1dfe
Lagiacrus24 <totzel24@yahoo.de>
2016-06-24 15:49:35 +0200

    Dummy FX-Startklasse

---
+++ src/main/java/tddt/Menu.java
@@ -0,0 +1,33 @@
+package tddt;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Menu extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//FXML laden
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("menu.fxml"));
+		
+		//Szenengröße bestimmen
+		Scene scene = new Scene(root,800 ,500);
+		//CSS laden
+		scene.getStylesheets().add(getClass().getClassLoader().getResource("menu.css").toExternalForm());
+
+		//Fenster anzeigen
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("TDDT");
+	    primaryStage.centerOnScreen();
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
