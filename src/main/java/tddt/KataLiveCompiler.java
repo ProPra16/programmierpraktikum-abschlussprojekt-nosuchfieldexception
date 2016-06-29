51f911c84a99388625cb2be57f2dd8f8e5efd1eb
Lagiacrus24 <totzel24@yahoo.de>
2016-06-29 18:39:51 +0200

    Hauptkatamethode implementiert

--- src/main/java/tddt/KataLiveCompiler.java
+++ src/main/java/tddt/KataLiveCompiler.java
@@ -5,13 +5,13 @@ import vk.core.api.CompilerFactory;
 import vk.core.api.JavaStringCompiler;
 
 public class KataLiveCompiler {
-	
-	public boolean doesThisCompile(String classname, String sourcecode){
-		CompilationUnit classToCompile = new CompilationUnit(classname, sourcecode, false);
-		JavaStringCompiler compiler = CompilerFactory.getCompiler(classToCompile);
+
+	public boolean codeFulfillsTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
+		CompilationUnit codeClass = new CompilationUnit(codeClassName, codeClassSourcecode, false);
+		CompilationUnit testClass = new CompilationUnit(testClassName, testClassSourcecode, false);
+		JavaStringCompiler compiler = CompilerFactory.getCompiler(codeClass, testClass);
 		compiler.compileAndRunTests();
-		compiler.getCompilerResult();
-		return !compiler.getCompilerResult().hasCompileErrors();
+		//Verursacht eine NPE falls Kompilierfehler auftreten
+		return compiler.getTestResult().getNumberOfFailedTests() == 0;
 	}
-	
 }
--- src/test/java/tddt/KataLiveCompilerTest.java
+++ src/test/java/tddt/KataLiveCompilerTest.java
@@ -6,9 +6,24 @@ import org.junit.Test;
 
 public class KataLiveCompilerTest {
 
-	@Test public void compilerCompilesCorrectClass() {
-        KataLiveCompiler compiler = new KataLiveCompiler();
-        assertTrue("correct class should return 'true'", compiler.doesThisCompile("Test", "public class Test {}"));
-    }
+	@Test public void compilesAndTestsCorrectClassesAndCorrectTests(){
+		KataLiveCompiler compiler = new KataLiveCompiler();
+		boolean correct = compiler.codeFulfillsTests("TwentyFour",
+														"public class TwentyFour { \n"
+															+ " public static int twentyFour() { \n"
+															+ "    return 24; \n"
+															+ " }\n"
+															+ "}",
+													"TwentyFourTest",
+														"import static org.junit.Assert.*;\n"
+															+ "import org.junit.Test;\n"
+															+ "public class TwentyFourTest { \n"
+															+ "   @Test\n"
+															+ "   public void isItReallyTwentyFour() { \n "
+															+ "       assertEquals(24, TwentyFour.twentyFour()); \n"
+															+ "   }\n "
+															+ "}");
+		assertTrue("correct class and correct tests should return 'true'", correct);
+	}
 	
 }
