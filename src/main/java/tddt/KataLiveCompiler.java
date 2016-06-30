319a4c89632baccd79ddfed263b9056e77d26232
Lagiacrus24 <totzel24@yahoo.de>
2016-06-30 09:18:34 +0200

    Neue Methode + zugehöriger Test

--- src/main/java/tddt/KataLiveCompiler.java
+++ src/main/java/tddt/KataLiveCompiler.java
@@ -6,12 +6,47 @@ import vk.core.api.JavaStringCompiler;
 
 public class KataLiveCompiler {
 
-	public boolean codeFulfillsTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
+	/**
+	 * Compiles the two given sources and tests if all tests are fulfilled.
+	 * Useful for the GREEN-Phase.
+	 * @param codeClassName The name of the class to test
+	 * @param codeClassSourcecode The sourcecode of the class to test
+	 * @param testClassName The name of the class which contains the tests
+	 * @param testClassSourcecode The sourcecode of the class which contains the tests
+	 * @return True if and only if both codes have no compiler errors and all tests are satisfied.
+	 */
+	public boolean codeCompilesAndFulfillsTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
+		JavaStringCompiler compiler = this.createCompilerAndCompiler(codeClassName, codeClassSourcecode, testClassName, testClassSourcecode);
+		return !compiler.getCompilerResult().hasCompileErrors() && compiler.getTestResult().getNumberOfFailedTests() == 0;
+	}
+	
+	/**
+	 * Compiles the two given sources and tests if all tests are not fulfilled.
+	 * Useful for the RED-Phase.
+	 * @param codeClassName The name of the class to test
+	 * @param codeClassSourcecode The sourcecode of the class to test
+	 * @param testClassName The name of the class which contains the tests
+	 * @param testClassSourcecode The sourcecode of the class which contains the tests
+	 * @return True if and only if both codes have no compiler errors and some tests are not satisfied.
+	 */
+	public boolean codeCompilesAndDoesNotFulfillTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
+		JavaStringCompiler compiler = this.createCompilerAndCompiler(codeClassName, codeClassSourcecode, testClassName, testClassSourcecode);
+		return !compiler.getCompilerResult().hasCompileErrors() && compiler.getTestResult().getNumberOfFailedTests() > 0;
+	}
+	
+	/**
+	 * Compiles the two given sources, runs the tests and hands over the compiler.
+	 * @param codeClassName The name of the class to test
+	 * @param codeClassSourcecode The sourcecode of the class to test
+	 * @param testClassName The name of the class which contains the tests
+	 * @param testClassSourcecode The sourcecode of the class which contains the tests
+	 * @return The compiler which already has run the code.
+	 */
+	private JavaStringCompiler createCompilerAndCompiler(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
 		CompilationUnit codeClass = new CompilationUnit(codeClassName, codeClassSourcecode, false);
 		CompilationUnit testClass = new CompilationUnit(testClassName, testClassSourcecode, false);
 		JavaStringCompiler compiler = CompilerFactory.getCompiler(codeClass, testClass);
 		compiler.compileAndRunTests();
-		//Verursacht eine NPE falls Kompilierfehler auftreten
-		return compiler.getTestResult().getNumberOfFailedTests() == 0;
+		return compiler;
 	}
 }
--- src/test/java/tddt/KataLiveCompilerTest.java
+++ src/test/java/tddt/KataLiveCompilerTest.java
@@ -8,7 +8,7 @@ public class KataLiveCompilerTest {
 
 	@Test public void compilesAndTestsCorrectClassesAndCorrectTests(){
 		KataLiveCompiler compiler = new KataLiveCompiler();
-		boolean correct = compiler.codeFulfillsTests("TwentyFour",
+		boolean correct = compiler.codeCompilesAndFulfillsTests("TwentyFour",
 														"public class TwentyFour { \n"
 															+ " public static int twentyFour() { \n"
 															+ "    return 24; \n"
@@ -24,6 +24,26 @@ public class KataLiveCompilerTest {
 															+ "   }\n "
 															+ "}");
 		assertTrue("correct class and correct tests should return 'true'", correct);
+	}
+	
+	@Test public void compilesAndTestsCorrectClassesAndWrongTests(){
+		KataLiveCompiler compiler = new KataLiveCompiler();
+		boolean wrong = compiler.codeCompilesAndDoesNotFulfillTests("TwentyFour",
+														"public class TwentyFour { \n"
+															+ " public static int twentyFour() { \n"
+															+ "    return 24; \n"
+															+ " }\n"
+															+ "}",
+													"TwentyFourTest",
+														"import static org.junit.Assert.*;\n"
+															+ "import org.junit.Test;\n"
+															+ "public class TwentyFourTest { \n"
+															+ "   @Test\n"
+															+ "   public void isItReallyTwentyFour() { \n "
+															+ "       assertEquals(27, TwentyFour.twentyFour()); \n"
+															+ "   }\n "
+															+ "}");
+		assertTrue("correct class and wrong tests should return 'false'", !wrong);
 	}
 	
 }
