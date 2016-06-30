7fc38c0962df9c994eb124287dd7fd86102932ce
Lagiacrus24 <totzel24@yahoo.de>
2016-06-30 09:56:05 +0200

    KataCompiler wächst
    - Mehr Funktionalität & Tests
    - Verbesserter Code & Lesbarkeit
    - Javadoc.

--- src/main/java/tddt/KataLiveCompiler.java
+++ src/main/java/tddt/KataLiveCompiler.java
@@ -1,52 +1,72 @@
 package tddt;
 
+import java.util.Collection;
+
 import vk.core.api.CompilationUnit;
+import vk.core.api.CompileError;
 import vk.core.api.CompilerFactory;
+import vk.core.api.CompilerResult;
 import vk.core.api.JavaStringCompiler;
 
 public class KataLiveCompiler {
-
+	
+	private JavaStringCompiler compiler;
+	private CompilationUnit codeClass, testClass;
+	
 	/**
-	 * Compiles the two given sources and tests if all tests are fulfilled.
-	 * Useful for the GREEN-Phase.
+	 * Creates a new Instance of KataLiveCompiler, compiles the code and runs the tests.
 	 * @param codeClassName The name of the class to test
 	 * @param codeClassSourcecode The sourcecode of the class to test
 	 * @param testClassName The name of the class which contains the tests
 	 * @param testClassSourcecode The sourcecode of the class which contains the tests
+	 */
+	public KataLiveCompiler(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
+		codeClass = new CompilationUnit(codeClassName, codeClassSourcecode, false);
+		testClass = new CompilationUnit(testClassName, testClassSourcecode, false);
+		compiler = CompilerFactory.getCompiler(codeClass, testClass);
+		compiler.compileAndRunTests();
+	}
+
+	/**
+	 * Compiles the two given sources and tests if all tests are fulfilled.
+	 * Useful for the GREEN-Phase.
 	 * @return True if and only if both codes have no compiler errors and all tests are satisfied.
 	 */
-	public boolean codeCompilesAndFulfillsTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
-		JavaStringCompiler compiler = this.createCompilerAndCompiler(codeClassName, codeClassSourcecode, testClassName, testClassSourcecode);
+	public boolean codeCompilesAndFulfillsTests(){
 		return !compiler.getCompilerResult().hasCompileErrors() && compiler.getTestResult().getNumberOfFailedTests() == 0;
 	}
 	
 	/**
 	 * Compiles the two given sources and tests if all tests are not fulfilled.
 	 * Useful for the RED-Phase.
-	 * @param codeClassName The name of the class to test
-	 * @param codeClassSourcecode The sourcecode of the class to test
-	 * @param testClassName The name of the class which contains the tests
-	 * @param testClassSourcecode The sourcecode of the class which contains the tests
 	 * @return True if and only if both codes have no compiler errors and some tests are not satisfied.
 	 */
-	public boolean codeCompilesAndDoesNotFulfillTests(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
-		JavaStringCompiler compiler = this.createCompilerAndCompiler(codeClassName, codeClassSourcecode, testClassName, testClassSourcecode);
+	public boolean codeCompilesAndDoesNotFulfillTests(){
 		return !compiler.getCompilerResult().hasCompileErrors() && compiler.getTestResult().getNumberOfFailedTests() > 0;
 	}
 	
 	/**
-	 * Compiles the two given sources, runs the tests and hands over the compiler.
-	 * @param codeClassName The name of the class to test
-	 * @param codeClassSourcecode The sourcecode of the class to test
-	 * @param testClassName The name of the class which contains the tests
-	 * @param testClassSourcecode The sourcecode of the class which contains the tests
-	 * @return The compiler which already has run the code.
+	 * Creates a big String containing all errors Messages that ocurred.
+	 * @return A String containing all compiler errors or, if there were none a positive message
 	 */
-	private JavaStringCompiler createCompilerAndCompiler(String codeClassName, String codeClassSourcecode, String testClassName, String testClassSourcecode){
-		CompilationUnit codeClass = new CompilationUnit(codeClassName, codeClassSourcecode, false);
-		CompilationUnit testClass = new CompilationUnit(testClassName, testClassSourcecode, false);
-		JavaStringCompiler compiler = CompilerFactory.getCompiler(codeClass, testClass);
-		compiler.compileAndRunTests();
-		return compiler;
+	public String getErrors(){
+		CompilerResult result = compiler.getCompilerResult();
+		if(result.hasCompileErrors()){
+			String errorString = "";
+			//Fetch codeClass compile-errors
+			Collection<CompileError> codeClassErrors = result.getCompilerErrorsForCompilationUnit(codeClass);
+			for(CompileError error : codeClassErrors){
+				errorString += error.getMessage();
+			}
+			//Fetch testClass compile-errors
+			Collection<CompileError> testClassErrors = result.getCompilerErrorsForCompilationUnit(testClass);
+			for(CompileError error : testClassErrors){
+				errorString += error.getMessage();
+			}
+			return errorString;
+		}else{
+			return "No compile-error detected, good job! :D";
+		}
 	}
+
 }
