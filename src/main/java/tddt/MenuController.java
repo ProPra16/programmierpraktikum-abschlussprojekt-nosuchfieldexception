14a1bf1bf199e6e85eaccef60efecfcd04563014
Julian Winter <JulianWinter1124@web.de>
2016-07-02 14:35:37 +0200

    Small changes and additions. Entry for B.S. made

--- src/main/java/tddt/MenuController.java
+++ src/main/java/tddt/MenuController.java
@@ -1,33 +1,47 @@
 package tddt;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MenuController {
-	
+
 	@FXML
 	private TextArea codeArea;
-	
+
 	@FXML
 	private TextArea testArea;
-	
+
 	@FXML
 	private TextArea errorArea;
 	
+	@FXML
+	private CheckBox babystepsCheckBox;
 	
 	@FXML
-	public void handleLoadButton(){
-		
-	}
-	
+	private TextField timeTextField;
+
 	@FXML
-	public void handleSaveButton(){
+	public void handleLoadButton() {
 
 	}
-	
+
 	@FXML
-	public void handleNextStepButton(){
-		
+	public void handleSaveButton() {
+	}
+
+	@FXML
+	public void handleNextStepButton() {
+
+	}
+
+	@FXML
+	public void handleStartButton() {
+		if(babystepsCheckBox.isSelected()) { //Babysteps!
+		} else { //No Babysteps
+			
+		}
 	}
 
 }
