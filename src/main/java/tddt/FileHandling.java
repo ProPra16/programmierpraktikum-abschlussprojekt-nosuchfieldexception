4f137774079d68c9b5237591ffef7eb9209251de
Fabio <fabso96@gmail.com>
2016-07-02 10:12:11 +0200

    Test-Commit

--- src/main/java/tddt/FileHandling.java
+++ src/main/java/tddt/FileHandling.java
@@ -1,5 +1,14 @@
 package tddt;
 
-public class FileHandling {
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+public class FileHandling {
+	
+	public static void openFile(){
+		Stage stage = new Stage();
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		fileChooser.showOpenDialog(stage);
+	}
 }
