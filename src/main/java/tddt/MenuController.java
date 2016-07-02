175da30946eb8dbf6fe2df49366faa0e326237f1
Lagiacrus24 <totzel24@yahoo.de>
2016-07-02 13:07:54 +0200

    Controller hinzugefügt

---
+++ src/main/java/tddt/MenuController.java
@@ -0,0 +1,33 @@
+package tddt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class MenuController {
+	
+	@FXML
+	private TextArea codeArea;
+	
+	@FXML
+	private TextArea testArea;
+	
+	@FXML
+	private TextArea errorArea;
+	
+	
+	@FXML
+	public void handleLoadButton(){
+		
+	}
+	
+	@FXML
+	public void handleSaveButton(){
+
+	}
+	
+	@FXML
+	public void handleNextStepButton(){
+		
+	}
+
+}
